ee79a8ffaf895b3733dac15e4f13566e4e3c1368
SADIK Brahim <sadbrah@gmail.com>
2025-07-09 16:22:57 +0100

    Implemented the Transaction class

---
+++ src/main/java/com/brahimsadik/personalFinanceTracker/Transaction.java
@@ -0,0 +1,68 @@
+package com.brahimsadik.personalFinanceTracker;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Transaction {
+    public
+        long id;
+        String description;
+        double amount;
+        LocalDate date;
+        TransactionType type;
+        String category; // (e.g., "Food", "Salary", "Rent")
+
+    public Transaction() {
+        this.id = new Date().getTime();
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public double getAmount() {
+        return this.amount;
+    }
+
+    public LocalDate getDate() {
+        return this.date;
+    }
+
+    public TransactionType getType() {
+        return this.type;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public Transaction setDescription(String desc) {
+        this.description = desc;
+        return this;
+    }
+
+    public Transaction setAmount(double amt) {
+        this.amount = amt;
+        return this;
+    }
+
+    public Transaction setDate(LocalDate date) {
+        this.date = date;
+        return this;
+    }
+
+    public Transaction setType(TransactionType type) {
+        this.type = type;
+        return this;
+    }
+
+    public Transaction setCategory(String cat) {
+        this.category = cat;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction ID: " + this.id;
+    }
+}
---
+++ src/main/java/com/brahimsadik/personalFinanceTracker/TransactionType.java
@@ -0,0 +1,6 @@
+package com.brahimsadik.personalFinanceTracker;
+
+public enum TransactionType {
+    INCOME,
+    EXPENSE
+}
